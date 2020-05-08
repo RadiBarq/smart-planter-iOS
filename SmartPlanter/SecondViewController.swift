3188e932626696ea39598948c3c3654ffce3d41b
radibarq <radi.barq@harri.com>
2020-05-09 00:17:47 +0300

    Initial Commit

---
+++ SmartPlanter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SmartPlanter
+//
+//  Created by Harri on 5/9/20.
+//  Copyright © 2020 Harri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
